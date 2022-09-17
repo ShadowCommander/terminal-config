55e9e8a5fd75cff89c93dd9943cb4120d7483111
ShadowCommander <10494922+ShadowCommander@users.noreply.github.com>
2022-09-17 14:30:07 -0700

    Add clipboard copy to GPG key

--- setup-git.sh
+++ setup-git.sh
@@ -16,16 +16,32 @@ setup_git_input() {
 
 setup_git_gpg() {
 	GPG_KEY=$(gpg --list-secret-keys --keyid-format=long | grep sec | sed -r 's:^.*/([A-Z0-9]+) .*$:\1:')
+	if [ -z "$GPG_KEY" ]
+	then
+		return
+	fi
 	git config --global user.signingkey $GPG_KEY
 	git config --global commit.gpgsign true
-	gpg --armor --export $GPG_KEY
+	echo 'Copying GPG key to clipboard.'
+	GPG_ARMOR=$(gpg --armor --export $GPG_KEY)
+	if command -v xclip &> /dev/null then
+		echo $GPG_ARMOR | xclip
+	elif command -v pbcopy &> /dev/null then
+		echo $GPG_ARMOR | pbcopy
+	elif command -v clip &> /dev/null then
+		echo $GPG_ARMOR | clip
+	elif command -v termux-clipboard-set &> /dev/null then
+		echo $GPG_ARMOR | termux-clipboard-set
+	else
+		echo 'Cannot find clipboard (xclip, pbcopy, clip, termux-clipboard-set)'
+	fi
 }
 
 # Check if the function exists (bash specific)
-if declare -f "$1" > /dev/null
+if declare -f "setup_git_$1" > /dev/null
 then
   # call arguments verbatim
-  "$@"
+  "setup_git_$@"
 else
   # Show a helpful error
   echo "'$1' is not a known function name" >&2
