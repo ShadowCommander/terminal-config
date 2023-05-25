766c54cde7453607c32193304edf0dc026cae930
ShadowCommander <10494922+ShadowCommander@users.noreply.github.com>
2023-05-24 18:46:51 -0700

    Fix Trilium installation on Termux
    pngquant needs to use an older version 5.0.2. Might be able to use a
    newer one. Needs testing.
    
    pngquant requires libpng

--- trilium-install.sh
+++ trilium-install.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 # Have to use nodejs-lts because nodejs 19 is not supported by Trilium
-pkg install binutils nodejs-lts python build-essential --no-install-recommends -y
+pkg install --no-install-recommends -y libpng binutils nodejs-lts python build-essential
 
-git clone https://github.com/zadam/trilium.git
-cd trilium
+mkdir -P $HOME/github
+git clone https://github.com/zadam/trilium.git $HOME/github/trilium
+cd $HOME/github/trilium
 
 # Add if check for if cpuInfos is undefined
 git apply --verbose 0001-Add-an-undefined-check-for-cpuInfos.patch
@@ -13,7 +14,7 @@ mv package-lock.json package-lock.json.bak
 sed -i.bak '/electron/d' package.json
 
 npm run webpack
-npm install pngquant-bin --ignore-scripts
+npm install pngquant-bin@5.0.2 --ignore-scripts
 npm install
 npm rebuild
 
